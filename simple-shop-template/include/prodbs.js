50fb6b8963fefca7744517bd5d6c3cb7bc09496f
xCris290x <christopher.mendez02@utrgv.edu>
2023-10-28 19:36:14 -0500

    Pushed

---
+++ simple-shop-template/include/prodbs.js
@@ -0,0 +1,30 @@
+
+var prodb = [{
+		id : "doll1",
+		pname: "Portable EV Charger",
+		price: 500.00,
+		image:"./img/charger.jpg",
+		rating: 5
+	},
+	{
+		id : "doll2",
+		pname: "LiFePO4",
+		price: 300.00,
+		image:"./img/battery.jpg",
+		rating: 4.9
+	},
+	{
+		id : "gun1",
+		pname: "Cable",
+		price: 20.00,
+		image: "./img/cables.jpg",
+		rating: 4.8
+	},
+	{
+		id : "other",
+		pname: "Connector",
+		price: 35.00,
+		image: "./img/connector.jpg",
+		rating: 5
+	}
+]
